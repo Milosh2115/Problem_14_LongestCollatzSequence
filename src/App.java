0853b82bdb23c51846d331f1615d66818be4c41e
Milosh2115 <miloszgrudziecki1@gmail.com>
2023-04-09 17:22:17 +0200

    first commit

---
+++ src/App.java
@@ -0,0 +1,64 @@
+public class App {
+
+
+    public static boolean oddoreven (int interpretednumber) {
+        boolean iseven = true;
+        if (interpretednumber % 2 == 0) {
+            iseven = true;
+        } else {
+            iseven = false;
+        }
+        return iseven;
+    }
+
+
+    public static int evencalc (int evenumber) {
+        int evenresult = evenumber / 2;
+        return evenresult;
+    }
+    
+
+    public static int oddcalc (int oddnumber) {
+        int casche = 3 * oddnumber;
+        int oddresult = casche + 1;
+        return oddresult;
+    }
+
+    
+    public static int chaincalc (int Collatznumber) {
+            int chain_l1 = 0;
+            if (Collatznumber == 1) { 
+            chain_l1 = 1;
+            }else if (Collatznumber <= 1000000) {
+                int interpretednumber = Collatznumber;
+                int chain_l = 0;
+                do {
+                    if (oddoreven(interpretednumber) == true) {
+                        interpretednumber = evencalc(interpretednumber);
+                        chain_l ++;
+                    }else {
+                        interpretednumber = oddcalc(interpretednumber);
+                        chain_l ++;
+                    };
+                }while (interpretednumber > 1);
+                chain_l1 = chain_l + 1;
+            }
+            return chain_l1;
+        }
+    public static void main(String[] args) {
+        int Collatznumber = 1;
+        int collectorarray[] = new int[1000000];
+        for (int i = 0; i < 1000000; i++) {
+            collectorarray[i] = chaincalc (Collatznumber);
+            Collatznumber ++;
+        }
+        int indexmax = 0;
+        for (int k = 0; k < collectorarray.length; k++) {
+            if (collectorarray[k] > collectorarray[indexmax]) {
+                indexmax = k;
+            }
+        }
+        int hihgesTST = indexmax + 1;
+        System.out.println(hihgesTST);
+    }
+    }
